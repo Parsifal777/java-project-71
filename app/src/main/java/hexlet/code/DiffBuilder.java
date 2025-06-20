255b587e89376aff8edc3b456c55c192e712d053
Иванов Глеб <gleb4832@mail.ru>
2025-06-20 10:00:52 +0400

    Changing permission of gradlew

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -1,9 +1,10 @@
 package hexlet.code;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
+import java.util.Set;
+import java.util.TreeSet;
 
 class DiffBuilder {
     public static Map<String, DiffNode> build(Map<String, Object> data1, Map<String, Object> data2) {
@@ -11,12 +12,12 @@ class DiffBuilder {
         Objects.requireNonNull(data2, "Second data map cannot be null");
 
         Map<String, DiffNode> diff = new TreeMap<>();
-        Map<String, Boolean> allKeys = new HashMap<>();
+        Set<String> allKeys = new TreeSet<>();
 
-        data1.keySet().forEach(key -> allKeys.put(key, true));
-        data2.keySet().forEach(key -> allKeys.put(key, true));
+        allKeys.addAll(data1.keySet());
+        allKeys.addAll(data2.keySet());
 
-        for (String key : allKeys.keySet()) {
+        for (String key : allKeys) {
             Object value1 = data1.get(key);
             Object value2 = data2.get(key);
 
@@ -24,13 +25,19 @@ class DiffBuilder {
                 diff.put(key, new DiffNode(DiffStatus.ADDED, null, value2));
             } else if (!data2.containsKey(key)) {
                 diff.put(key, new DiffNode(DiffStatus.REMOVED, value1, null));
-            } else if (Objects.equals(value1, value2)) {
-                diff.put(key, new DiffNode(DiffStatus.UNCHANGED, value1, value1));
+            } else if (deepEquals(value1, value2)) {
+                diff.put(key, new DiffNode(DiffStatus.UNCHANGED, value1, value2));
             } else {
                 diff.put(key, new DiffNode(DiffStatus.CHANGED, value1, value2));
             }
         }
 
         return diff;
+    }
+
+    private static boolean deepEquals(Object a, Object b) {
+        if (a == b) return true;
+        if (a == null || b == null) return false;
+        return Objects.deepEquals(a, b);
     }
 }
